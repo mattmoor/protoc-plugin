964ce6691a87ce7491c8752fefccb98196a1aad0
Matt Moore <mattmoor@google.com>
2019-01-01 16:13:56 +0000

    Use a hard-coded protoc install path.
    This works around some funniness trying to run this on my corp setup.
    
    Also fix a copy/paste bug in one of the go:generate's which wasn't installing krpc-go

--- example/doc.go
+++ example/doc.go
@@ -2,7 +2,7 @@ package mattmoor
 
 //go:generate go generate github.com/mattmoor/protoc-plugin/proto
 //go:generate go install github.com/mattmoor/protoc-plugin/cmd/krpc-yaml
-//go:generate go install github.com/mattmoor/protoc-plugin/cmd/krpc-yaml
+//go:generate go install github.com/mattmoor/protoc-plugin/cmd/krpc-go
 //go:generate mkdir -p ./build
-//go:generate protoc -I${GOPATH}/src -I. --plugin=protoc-gen-krpc-yaml=${GOPATH}/bin/krpc-yaml --krpc-yaml_opt=github.com/blurg/bleh/cmd/thing --krpc-yaml_out=./build hello.proto
-//go:generate protoc -I${GOPATH}/src -I. --plugin=protoc-gen-krpc-go=${GOPATH}/bin/krpc-go --krpc-go_out=./build hello.proto
+//go:generate ${HOME}/protoc-3.6.1/bin/protoc -I${HOME}/protoc-3.6.1/include -I${GOPATH}/src -I. --plugin=protoc-gen-krpc-yaml=${GOPATH}/bin/krpc-yaml --krpc-yaml_opt=github.com/blurg/bleh/cmd/thing --krpc-yaml_out=./build hello.proto
+//go:generate ${HOME}/protoc-3.6.1/bin/protoc -I${HOME}/protoc-3.6.1/include -I${GOPATH}/src -I. --plugin=protoc-gen-krpc-go=${GOPATH}/bin/krpc-go --krpc-go_out=./build hello.proto
--- proto/doc.go
+++ proto/doc.go
@@ -2,4 +2,4 @@ package proto
 
 //go:generate go get -u github.com/golang/protobuf/protoc-gen-go
 //go:generate mkdir -p ./krpc
-//go:generate protoc --plugin=protoc-gen-go=${GOPATH}/bin/protoc-gen-go --go_out=./krpc krpc.proto
+//go:generate ${HOME}/protoc-3.6.1/bin/protoc -I${HOME}/protoc-3.6.1/include -I. --plugin=protoc-gen-go=${GOPATH}/bin/protoc-gen-go --go_out=./krpc krpc.proto
