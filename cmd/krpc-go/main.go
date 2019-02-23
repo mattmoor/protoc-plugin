0f98a81d156d7908b38ff1d212f023c53abe422d
Matt Moore <mattmoor@google.com>
2019-02-23 04:59:23 +0000

    Starting to add Go codegen into the mix via TODO

--- cmd/krpc-go/main.go
+++ cmd/krpc-go/main.go
@@ -6,13 +6,9 @@ import (
 	"os"
 	"strings"
 
-	"github.com/golang/protobuf/protoc-gen-go/plugin"
 	"github.com/golang/protobuf/protoc-gen-go/descriptor"
+	"github.com/golang/protobuf/protoc-gen-go/plugin"
 )
-
-func method(fd *descriptor.FileDescriptorProto, sdp *descriptor.ServiceDescriptorProto, mdp *descriptor.MethodDescriptorProto) (string, error) {
-        return "// TODO: " + fd.GetPackage() + "/" + sdp.GetName() + "/" + mdp.GetName(), nil
-}
 
 func doit(request *plugin_go.CodeGeneratorRequest) (*plugin_go.CodeGeneratorResponse, error) {
 	codegen := make(map[string]struct{})
@@ -26,40 +22,56 @@ func doit(request *plugin_go.CodeGeneratorRequest) (*plugin_go.CodeGeneratorResp
 			continue
 		}
 
-		mainParts := []string{
-			  "package main",
-			  `import "log"`,
-			  "func main() {",
-		}
-
 		for _, sdp := range fd.Service {
-		        for _, mdp := range sdp.Method {
-			        content, err := method(fd, sdp, mdp)
-				if err != nil {
-				   return nil, err
-				}
-				mainParts = append(mainParts, content)
+			// TODO(mattmoor): Add pb import
+
+			// TODO(mattmoor): Generate a class definition.
+			// e.g. type ServiceName struct {}
+
+			// TODO(mattmoor): Generate a service registration.
+			// e.g. svcName := &ServiceName{}
+			//      pb.RegisterServiceNameServer(grpcServer, sycName)
+
+			for _, mdp := range sdp.Method {
+				// TODO(mattmoor): Generate methods off of the class definition.
+				// e.g. non-streaming:
+				//   func (s *ServiceName) MethodName(ctx context.Context, req *pb.MethodRequest) (*pb.MethodResponse, error) {
+				//      // Implemented by the user.
+				//      return someimportpath.MethodName(ctx, req)
+				//   }
+				//
+				// e.g. streaming:
+				//   func (s *ServiceName) MethodName(stream pb.ServiceName_MethodNameServer) error {
+				//      // Implemented by the user.
+				//      return someimportpath.MethodName(stream)
+				//   }
+
+				// "// TODO: " + fd.GetPackage() + "/" + sdp.GetName() + "/" + mdp.GetName(), nil
+
+				// TODO(mattmoor): Consider optionally retrieving the import path for this
+				// method from an annotation?
 			}
 		}
 
-		mainParts = append(mainParts,
-			  `	log.Fatal("NYI")`,
-			  "}",
-		)
 		mainName := "main.go"
-		mainContent := strings.Join(mainParts, "\n")
+		mainContent := fmt.Sprintf(
+			mainTemplate,
+			importpath,
+			serviceDefinitions,
+			registerServices,
+		)
 
 		log.Printf("Content[%q] = %s", mainName, mainContent)
 		resp.File = append(resp.File, &plugin_go.CodeGeneratorResponse_File{
-			  Name:    &mainName,
-			  Content: &mainContent,
+			Name:    &mainName,
+			Content: &mainContent,
 		})
 	}
 	return &resp, nil
 }
 
 func main() {
-        log.Printf("Args: %v", os.Args)
+	log.Printf("Args: %v", os.Args)
 	bytes, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		log.Fatalf("Unable to read stdin: %v", err)
@@ -70,7 +82,7 @@ func main() {
 		log.Fatalf("Unable to unmarshal codegen request: %v", err)
 	}
 
-	log.Printf("Parameter: %q", req.GetParameter())
+	// log.Printf("Parameter: %q", req.GetParameter())
 	resp, err := doit(&req)
 	if err != nil {
 		// Return plugin errors as an error response instead of a
@@ -87,3 +99,43 @@ func main() {
 	}
 	os.Stdout.Write(result)
 }
+
+const (
+	mainTemplate = `
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+	"os"
+
+	// Inject import paths for relevant definitions.
+	%s
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+// Inject service structs and methods here.
+%s
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%%d", os.Getenv("PORT")))
+	if err != nil {
+		log.Fatalf("failed to listen: %%v", err)
+	}
+
+	// The grpcServer is currently configured to serve h2c traffic by default.
+	// To configure credentials or encryption, see: https://grpc.io/docs/guides/auth.html#go
+	grpcServer := grpc.NewServer()
+
+	// Inject service registration here.
+	%s
+
+	grpcServer.Serve(lis)
+}
+`
+)
--- example/doc.go
+++ example/doc.go
@@ -4,5 +4,6 @@ package mattmoor
 //go:generate go install github.com/mattmoor/protoc-plugin/cmd/krpc-yaml
 //go:generate go install github.com/mattmoor/protoc-plugin/cmd/krpc-go
 //go:generate mkdir -p ./build
-//go:generate ${HOME}/protoc-3.6.1/bin/protoc -I${HOME}/protoc-3.6.1/include -I${GOPATH}/src -I. --plugin=protoc-gen-krpc-yaml=${GOPATH}/bin/krpc-yaml --krpc-yaml_opt=docker.io/mattmoor/grpc-ping-go --krpc-yaml_out=./build hello.proto
-//go:generate ${HOME}/protoc-3.6.1/bin/protoc -I${HOME}/protoc-3.6.1/include -I${GOPATH}/src -I. --plugin=protoc-gen-krpc-go=${GOPATH}/bin/krpc-go --krpc-go_out=./build hello.proto
+//go:generate ${HOME}/protoc-3.6.1/bin/protoc -I${HOME}/protoc-3.6.1/include -I${GOPATH}/src -I. --plugin=protoc-gen-go=${GOPATH}/bin/protoc-gen-go --go_out=./build hello.proto
+//go:generate ${HOME}/protoc-3.6.1/bin/protoc -I${HOME}/protoc-3.6.1/include -I${GOPATH}/src -I. --plugin=protoc-gen-krpc-yaml=${GOPATH}/bin/krpc-yaml --krpc-yaml_opt=docker.io/mattmoor/grpc-ping-go               --krpc-yaml_out=./build hello.proto
+//go:generate ${HOME}/protoc-3.6.1/bin/protoc -I${HOME}/protoc-3.6.1/include -I${GOPATH}/src -I. --plugin=protoc-gen-krpc-go=${GOPATH}/bin/krpc-go     --krpc-go_opt=github.com/mattmoor/protoc-plugin/example/build --krpc-go_out=./build hello.proto
